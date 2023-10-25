0b961c184b33fff47ffc91683d4fed2839f8660e
Albrin <albrinndr@gmail.com>
2023-10-25 15:54:58 +0530

    Updated navbar

--- src/components/user/NavBar.tsx
+++ src/components/user/NavBar.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import Logo from '../../assets/logo.svg';
 import MenuIcon from '../../assets/menu.svg';
+import MenuWhite from '../../assets/menu-white.svg';
 
 interface NavBarProps {
     color?: boolean;
     fixed?: boolean;
 }
-const NavBar: React.FC<NavBarProps> = ({ color,fixed }) => {
+const NavBar: React.FC<NavBarProps> = ({ color, fixed }) => {
     const [dropMenu, setDropMenu] = useState(false);
 
     const handleMenu = () => {
@@ -14,7 +15,9 @@ const NavBar: React.FC<NavBarProps> = ({ color,fixed }) => {
     };
     const navBarStyle = !color ? 'bg-white shadow  transition-all transition-all duration-1000 delay-2000 ' : '';
     const itemStyle = !color ? 'text-gray-800 ' : 'text-white';
-    const navPosition = fixed?'fixed':'sticky'
+    const navPosition = fixed ? 'fixed' : 'sticky';
+    const MenuIconType = !color ? MenuIcon : MenuWhite;
+    const underLineStyle = !color ? 'group-hover:bg-gray-800' : 'group-hover:bg-white';
     return (
         <div className={` right-0 left-0 top-0 z-50 px-4 md:px-14 ${navBarStyle} ${navPosition}`}>
             <div className="">
@@ -25,24 +28,39 @@ const NavBar: React.FC<NavBarProps> = ({ color,fixed }) => {
                     </div>
 
                     <div className="hidden md:flex md:items-center">
-                    {/* style={{ textShadow: '2px 2px 4px #000000' }} */}
-                        <a href="#" className={`${itemStyle} text-md  font-semibold hover:text-purple-600 mr-9`} >Home</a>
-                        <a href="#" className={`${itemStyle} text-md font-semibold hover:text-purple-600 mr-9`} >Fixtures</a>
-                        <a href="#" className={`${itemStyle} text-md font-semibold hover:text-purple-600 mr-9`} >Reviews</a>
-                        <a href="#" className={`${itemStyle} text-md font-semibold hover:text-purple-600 mr-9`} >Community</a>
-                        <a href="#" className={`${itemStyle} text-md font-semibold hover:text-purple-600 1`} >Search</a>
+                        <a href="#" className={`${itemStyle} text-md font-semibold relative mr-9 group`}>
+                            Home
+                            <span className={`absolute left-0 right-0 bottom-0 h-px top-7 ${underLineStyle} transition-all`}></span>
+                        </a>
+                        <a href="#" className={`${itemStyle} text-md font-semibold relative mr-9 group`}>
+                            Fixtures
+                            <span className={`absolute left-0 right-0 bottom-0 h-px top-7 ${underLineStyle} transition-all`}></span>
+                        </a>
+                        <a href="#" className={`${itemStyle} text-md font-semibold relative mr-9 group`}>
+                            Reviews
+                            <span className={`absolute left-0 right-0 bottom-0 h-px top-7 ${underLineStyle} transition-all`}></span>
+                        </a>
+                        <a href="#" className={`${itemStyle} text-md font-semibold relative mr-9 group`}>
+                            Community
+                            <span className={`absolute left-0 right-0 bottom-0 h-px top-7 ${underLineStyle} transition-all`}></span>
+                        </a>
+                        <a href="#" className={`${itemStyle} text-md font-semibold relative group`}>
+                            Search
+                            <span className={`absolute left-0 right-0 bottom-0 h-px top-7 ${underLineStyle} transition-all`}></span>
+                        </a>
                     </div>
 
+
                     <div className="hidden md:flex md:items-center">
-                        <a href="#" className={`${itemStyle} text-md font-semibold border px-4 py-2 rounded-lg hover:text-purple-600 hover:border-purple-6001`}>Log In</a>
+                        <a href="#" className={`${itemStyle} text-md font-semibold  px-4 py-2 rounded-lg  hover:bg-gray-400 hover:bg-opacity-25`}>Log In</a>
                     </div>
 
                     <div className="md:hidden cursor-pointer" onClick={handleMenu}>
-                        <img src={MenuIcon} alt="" width="30rem" />
+                        <img src={MenuIconType} alt="" width="30rem" />
                     </div>
                 </div>
 
-                {dropMenu && <div className="block md:hidden bg-white opacity-90 border-t-2 py-2">
+                {dropMenu && <div className="block md:hidden bg-white  border-t-2 py-2">
                     <div className="flex flex-col p-3">
                         <a href="#" className="text-gray-800 text-md font-semibold hover:text-purple-600 mb-1">Home</a>
                         <a href="#" className="text-gray-800 text-md font-semibold hover:text-purple-600 mb-1">Fixtures</a>
@@ -50,7 +68,7 @@ const NavBar: React.FC<NavBarProps> = ({ color,fixed }) => {
                         <a href="#" className="text-gray-800 text-md font-semibold hover:text-purple-600 mb-1">Community</a>
                         <a href="#" className="text-gray-800 text-md font-semibold hover:text-purple-600 mb-1">Search</a>
                         <div className="flex justify-between items-center border-t-2 pt-2">
-                            <a href="#" className="text-gray-800 text-md font-semibold border px-4 py-1 rounded-lg hover:text-purple-600 hover:border-purple-600">Log In</a>
+                            <a href="#" className="text-gray-800 text-md font-semibold border px-4 py-1 rounded-lg hover:bg-gray-400 hover:bg-opacity-25">Log In</a>
                         </div>
                     </div>
                 </div>}
