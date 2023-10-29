eb1e2ad65c069af26b5a96271c983a067935576c
Albrin <albrinndr@gmail.com>
2023-10-29 19:40:16 +0530

    Google dependencies configured

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,14 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './store/store.ts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const queryClient = new QueryClient();
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
+  <GoogleOAuthProvider clientId='203302473457-j5ga5pi4j4mk4chos3plqed0dptshsrc.apps.googleusercontent.com'>
     <Provider store={store}>
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
@@ -18,5 +21,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </QueryClientProvider>
       </BrowserRouter>
     </Provider>
+  </GoogleOAuthProvider>
   // </React.StrictMode>,
 );
