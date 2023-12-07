24846dd0cade2fd9bd451be8578cff031c3b0ae8
Albrin <albrinndr@gmail.com>
2023-12-07 19:44:58 +0530

    Base url changes

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = 'https://derbydome.website/api'
+const BASE_URL = import.meta.env.VITE_BASE_URL
 
 const Api = axios.create({ baseURL: BASE_URL, withCredentials: true });
 
